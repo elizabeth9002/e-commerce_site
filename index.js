21c0e4a5f49be0c89fe3a6ee5cb48e8fcedda625
elizabeth9002 <anasedaniella@gmail.com>
2023-12-15 17:31:55 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded',()=>{
+console.log('Script loaded!');
+
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+
+if (close) {
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+}
+});
+
+
